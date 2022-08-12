41ac8988db8d32c3a9a8a223f1986c5cd49e1fb1
Isaias <99995191+isaias30silva@users.noreply.github.com>
2022-08-12 20:57:53 -0300

    Add files via upload

---
+++ CRUD_LojaTudoDeBom/projeto-loja-tudo-de-bom/CRUD_LojaTudoDeBom/src/DTO/cadastroDTO.java
@@ -0,0 +1,32 @@
+package DTO;
+
+public class cadastroDTO {
+	private String nome_cliente, endereco_cliente;
+	private int id_cliente;
+
+	public int getId_cliente() {
+		return id_cliente;
+	}
+
+	public void setId_cliente(int id_cliente) {
+		this.id_cliente = id_cliente;
+	}
+
+	public String getNome_cliente() {
+		return nome_cliente;
+	}
+
+	public void setNome_cliente(String nome_cliente) {
+		this.nome_cliente = nome_cliente;
+	}
+
+	public String getEndereco_cliente() {
+		return endereco_cliente;
+	}
+
+	public void setEndereco_cliente(String endereco_cliente) {
+		this.endereco_cliente = endereco_cliente;
+	}
+	
+
+}
